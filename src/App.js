afb6c81773759c4d1b5dedb0ea1ebb9011b8fa82
Rusne <rusne.pet"gmail.com>
2020-11-26 19:05:47 +0200

    shop building

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Data from "./data.json";
-import Card from "./components/ProductCard";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Administration from "./components/Administration";
+import ProductListComponent from "./components/ProductListComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <main className="container">
-        <div className="row">
-          {/* get data which you need, unique keys for list  are must!
-          */}
-          {Data.map((post) => {
-            return (
-              <div className="col" key={post.id}>
-                <Card title={post.title} text={post.text} />
-              </div>
-            );
-          })}
-        </div>
-      </main>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+        <header className="">
+          <div>
+            <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <ProductListComponent />
+          </Route>
+          <Route path="/admin">
+            <Administration />
+          </Route>
+          <Route path="/product/:id">
+            <ProductListComponent />
+          </Route>
+          <Route path="*">
+            <h4 className="display-1">Not Found 404</h4>
+          </Route>
+          <Route>
+            <h4 className="display-1">Not Found 404</h4>
+          </Route>
+        </Switch>
+     
+</div>
+         </header>
+          </div>
+        <main className="container">
+          <div className="row">
+<ProductListComponent/>
+          </div>
+         
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { Switch, Redirect, Route } from "react-router";
+import { BrowserRouter, Link } from "react-router-dom";
+
+
+
+var NoMatch = (props) => {
+  var goApp = () => props.history.push("/");
+  return (
+    <div>
+      Route did not match
+      <button onClick={goApp}>Go Home</button>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> 
+      <App />
+       </BrowserRouter> 
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
+      // <Navbar>
+      //   <Switch>
+      //     <Route exact path="/" component={App} />
+      //     <Route path="/products/:id" component={Products} />
+      //     <Route path="/products" component={Products} />
+      //     <Route path="*" component={NoMatch} />
+      //     <Route component={NoMatch} />
+      //   </Switch>
+      // </Navbar> 
+   
 );
 
 // If you want to start measuring performance in your app, pass a function
